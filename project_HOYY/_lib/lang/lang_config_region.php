460ce08e75d120bdda365d37011ee9d325d5b4ba
Jeisonbernardo <123920422+jeisontaboada@users.noreply.github.com>
2024-02-05 12:43:47 -0500

    eliminar

--- project_HOYY/_lib/lang/lang_config_region.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $this->Nm_lang_conf_region = array();
-  $this->Nm_lang_conf_region['es;es_es'] = "Spanish/Español - Spanish (Spain)";
-  ksort($this->Nm_lang_conf_region);
-?>
--- project_HOYY/form_ciudades/form_ciudades.php
+++
@@ -1,3 +0,0 @@
-<?php 
-  include_once('../form_ciudades/index.php'); 
-?> 
