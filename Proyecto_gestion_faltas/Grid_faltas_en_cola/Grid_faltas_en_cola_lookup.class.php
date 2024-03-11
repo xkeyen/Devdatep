81110d441a796c8fd37b8bbe951ce0021f293c89
Jeisonbernardo <123920422+jeisontaboada@users.noreply.github.com>
2024-03-11 12:54:13 -0500

    vista faltas en cola

---
+++ Proyecto_gestion_faltas/Grid_faltas_en_cola/Grid_faltas_en_cola_lookup.class.php
@@ -0,0 +1,5 @@
+<?php
+class Grid_faltas_en_cola_lookup
+{
+}
+?>
---
+++ Proyecto_gestion_faltas/_lib/lang/lang_config_region.php
@@ -0,0 +1,6 @@
+<?php
+  $this->Nm_lang_conf_region = array();
+  $this->Nm_lang_conf_region['es;es_es'] = "Spanish/Español - Español (España)";
+  $this->Nm_lang_conf_region['en_us;en_us'] = "English - Inglés (Estados Unidos)";
+  ksort($this->Nm_lang_conf_region);
+?>
